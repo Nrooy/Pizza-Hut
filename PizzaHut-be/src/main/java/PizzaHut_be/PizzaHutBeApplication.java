45e6f49798e1d086f9a31fd5651d1f26a96acb08
Nrooy <duyhuyy1999@gmail.com>
2024-07-13 16:41:12 +0700

    13-07 crate base be

---
+++ PizzaHut-be/src/main/java/PizzaHut_be/PizzaHutBeApplication.java
@@ -0,0 +1,13 @@
+package PizzaHut_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaHutBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaHutBeApplication.class, args);
+	}
+
+}
